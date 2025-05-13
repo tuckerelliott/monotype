8f08d8e52dd7fc899ea290236c809fe3b19720ac
Tucker Elliott <telliott@adobe.com>
2025-05-13 18:54:10 -0400

    form styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -273,7 +273,7 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  import("../tools/sidekick/aem-experimentation.js");
+  // import("../tools/sidekick/aem-experimentation.js");
   const main = doc.querySelector("main");
   await loadBlocks(main);
 
